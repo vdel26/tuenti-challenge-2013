cfe11f5117c2cf8dc55274df4e76c591ea6e6588
Victor Delgado <vdel26@gmail.com>
2013-04-30 22:00:43 +0200

    changes to ex2

--- ex2.py
+++ ex2.py
@@ -3,8 +3,20 @@
 import fileinput
 
 
-def find(word):
-    pass
+def seek(word, dictname):
+    with open(dictname, 'r') as wordlist:
+        sword = sorted(word)
+        a = []
+        for entry in wordlist:
+            if word != entry.rstrip('\n'):
+                if sword == sorted(entry.rstrip('\n')):
+                    a.append(entry.rstrip('\n'))
+    return a
+
+
+def beautify(word, acronyms):
+    s_acronyms = sorted(acronyms)
+    print "%s -> %s" % (word, ' '.join(s_acronyms))
 
 
 if __name__ == '__main__':
@@ -13,10 +25,9 @@ if __name__ == '__main__':
         line_clean = line.rstrip('\n')
         if linenum == 2:
             dictname = str(line_clean)
-            print dictname
         elif linenum == 4:
             numwords = int(line_clean)
-            print numwords
         elif line_clean[0] is not '#':
             word = str(line_clean)
-            print "word"
+            acronyms = seek(word, dictname)
+            beautify(word, acronyms)
